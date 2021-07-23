74a4ed1696e8c6111b832c7250d63628b321868c
Degeneracy_Evil <h_p_chen26@163.com>
2021-07-23 16:36:12 +0800

    add file .gitignore and 命名规则.md

--- Tree/BinaryTree.h
+++ Tree/BinaryTree.h
@@ -1,18 +1,39 @@
-typedef struct binarytree
+#include <malloc.h>
+
+typedef struct _BinaryTree
 {
 	int data;
-	struct binarytree * parent;
-	struct binarytree * leftchild;
-	struct binarytree * rightchild;
+	struct _BinaryTree * parent;
+	struct _BinaryTree * leftchild;
+	struct _BinaryTree * rightchild;
 }BinaryTree;
 
-BinaryTree * __CreateBinaryTree();
+BinaryTree * __CreateBinaryTree(void);
 BinaryTree * CreateBinaryTree(int need);
 
 void InsertBinaryTree(BinaryTree * pTree, int target ,int node);
 void DeleteBinaryTree(BinaryTree * pTree, int target);
-void DeleteBinaryTreeAll(BinaryTree * pTree);
+void DeleteAllBinaryTree(BinaryTree * pTree);
 
-int GetBinaryTreeData(BinaryTree * pTree, int target);
-void PrintBinaryTreeAll(BinaryTree * pTree);
+int GetDataBinaryTree(BinaryTree * pTree, int target);
+void PrintAllBinaryTree(BinaryTree * pTree);
 
+
+
+BinaryTree * __CreateBinaryTree(void)
+{
+	return (BinaryTree *)malloc(sizeof(BinaryTree));
+}
+
+BinaryTree * CreateBinaryTree(int need)
+{
+	BinaryTree * pTree = __CreateBinaryTree();
+	BinaryTree * p = pTree;
+
+	for (int  i = 1; i < need; ++i)
+	{
+		;
+	}
+	
+	return p;
+}
